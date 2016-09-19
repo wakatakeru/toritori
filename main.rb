a08d7aa847288b96c850dc0b99deae5554263927
wakatakeru <shimizu.net421@gmail.com>
2016-09-19 13:08:57 +0900

    ツイートできるようにした

--- main.rb
+++ main.rb
@@ -3,12 +3,22 @@ require 'sinatra'
 require_relative 'setting'
 
 get '/' do
+  client = Client.new
+
   erb :index
+  client.user_timeline
+  
+=begin
+  client.timeline.each do |tweet|
+    print '<hr>'
+    print "#{tweet.user} | #{tweet.full_text}"
+  end
+=end
 end
 
 post '/send' do
   client = Client.new
   text = params[:message]
   client.update(text)
-  erb :index
+  redirect '/'
 end
